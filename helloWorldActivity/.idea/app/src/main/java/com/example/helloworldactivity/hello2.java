2de0a499403f4bdb0b23b2cc9ca306b4b64423ca
han_chentao <2017118109@stu.hstc.edu.cn>
2019-10-24 15:49:45 +0800

    浏览器，tel，地图

---
+++ helloWorldActivity/.idea/app/src/main/java/com/example/helloworldactivity/hello2.java
@@ -0,0 +1,33 @@
+package com.example.helloworldactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class hello2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello2_layout);
+        Button hello1=(Button) findViewById(R.id.hello1);
+        final Button hello2=(Button) findViewById(R.id.hello2);
+       hello2.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View view){
+               Intent intent=new Intent(Intent.ACTION_DIAL);
+               intent.setData(Uri.parse("tel:10086"));
+               startActivity(intent);
+           }});
+        hello1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent=new Intent(hello2.this,hello1.class);
+                startActivity(intent);
+            }});
+    }
+}
